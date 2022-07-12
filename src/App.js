228ed3ac486cdd682ce2cf4f8a2757f92d4c27a3
Bagus <bagus.nurhuda@ui.ac.id>
2022-07-12 11:03:39 +0700

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  const app = 10
+
   return (
     <div className="App container mx-auto">
-      <h1 className="font-weight text-2xl font my-5">Bagus Nurhuda - bagusn05@gmail.com - Associate Software Engineer </h1>
-      <div className="grid grid-cols-5 gap-4 mx-6">
+      <a className="font-weight text-2xl font my-5 cursor-pointer" href="http://bagusnurhuda.com">Bagus Nurhuda - bagusn05@gmail.com - Associate Software Engineer </a>
+      <div className="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 gap-4 mx-6">
       {[...Array(10)].map((x, i) =>
         <div className="max-w-sm rounded-xl shadow-lg container">
         <img className="w-full" src={require('./img/gambar1.jpg')} alt="Mountain" />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Living English Pioneer (1-Month Free)</div>
-          <p className="text-gray-700 text-base">
-            Oleh Sekolah.mu
-          </p>
+        <div className="px-4 pt-2 ">
+          <div className='flex justify-start'>
+            <div className="font-bold text-xl mb-2 ">Living English Pioneer (1-Month Free)</div>
+          </div>          
+          <div className='flex justify-start'>
+              <p className="text-gray-700 text-base">
+                Oleh Sekolah.mu
+              </p>
+            </div>
+          
         </div>
-        <div className="px-6 pt-4 pb-2">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #photography
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #travel
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #winter
+        <div className='flex justify-start items-center px-4'>
+          <div className='w-1/4 '>
+            <img className="justify-center" src={require('./img/bintang.png')} />
+          </div>
+          <span className="text-gray-400 font-medium pl-1">4.87</span>
+          <span className="text-gray-400 font-medium pl-1">(1051)</span>
+        </div>
+        <div className="px-4 pt-2 pb-2">
+          <span className="inline-block text-sm font-semibold  mr-2 mb-2 flex flex-start text-lg">
+            GRATIS
           </span>
         </div>
       </div>
